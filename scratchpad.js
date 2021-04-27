cbd0aa45714bcb31b5d2abd1068617ac8a3ad76e
SMC242 <56912943+SMC242@users.noreply.github.com>
2021-04-27 10:57:32 +0100

    Working on adding in the comma spam
    The replacement function is malfunctioning but it actually looks realistic

--- scratchpad.js
+++ scratchpad.js
@@ -2,9 +2,10 @@ const rand_int = (max_val) => Math.floor(Math.random() * max_val);
 const flatten_matches = (matches) => [...matches];
 const get_matches = (expr) => (to_match) =>
   flatten_matches(to_match.matchAll(expr));
-const replace_at = (to_add) => (start) => (str) =>
-  str.slice(0, start) + to_add + str.slice(start + to_add.length);
-const random_replace = (expr, replace_chance, to_add) => (to_check) => {
+// TODO: work out why unmatched characters get replaced
+const replace_at = (to_add) => (start) => (replace_length) => (str) =>
+  `${str.slice(0, start)}${to_add}${str.slice(start + replace_length)}`;
+const random_replace = (expr, replace_chance, get_to_add) => (to_check) => {
   const matches = get_matches(expr)(to_check);
   let result = to_check;
   matches.forEach((m) => {
@@ -12,18 +13,27 @@ const random_replace = (expr, replace_chance, to_add) => (to_check) => {
       console.log(`Skipping match: ${m}`);
       return;
     }
-    result =
-      replace_chance >= rand_int(100)
-        ? replace_at(to_add)(m.index)(to_check)
-        : result;
+    const should_replace = replace_chance >= rand_int(100);
+    const replace_str = get_to_add();
+    result = should_replace
+      ? replace_at(replace_str)(m.index)(m.length)(result)
+      : result;
   });
   return result;
 };
 
 const tester =
   "Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day 🦵";
-const replace_commas = random_replace(/[,]/g)(100)(" = ");
-console.log(replace_commas(tester));
+const replace_commas = random_replace(/[,]/g, 80, () => "=");
+const add_commas = random_replace(
+  /\s/g,
+  30,
+  () => new Array(rand_int(2) + 1).fill(",").join("") + " "
+);
+// TODO: remove ~10% of characters
+// TODO: replace 'fucking' with 'f,,' or 'facking'
+const mikeify = (str) => add_commas(replace_commas(str));
+
 // matches -> take {replace_chance} portion of matches -> replace those
 const linear_tester =
   "Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan on NC Miller, has the most ASP members, and carrys the NC. I'm very strong and never skip leg day :leg:Hello mateys, my name is Mike33. I hate the fucking poodlefit and am the leader of BHO which is the best clan";
@@ -47,5 +57,6 @@ function linear_search() {
 }
 
 const start = process.hrtime();
-console.log(linear_search());
+// console.log(linear_search());
+console.log(mikeify(tester)); // TODO: optimise performance to <0.5s
 console.log(`${process.hrtime(start)} seconds`);
